c5e0c98b27775ecb9f247e4813a09c772cbbd5d4
Marios Christodoulou <mchristodoulou@dgmedialink.onmicrosoft.com>
2023-08-02 11:16:25 +0300

    Sidebar Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { CoupensComponent } from './coupens/coupens.component';
+import { PagesComponent } from './pages/pages.component';
+import { MediaComponent } from './media/media.component';
+import { SettingsComponent } from './settings/settings.component';
+import { navbarData } from './sidenav/nav-data';
+
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent }, // Example route
+  { path: 'products', component: ProductsComponent }, // Example route
+  // Define other routes here
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    SidenavComponent,
+    DashboardComponent,
+    ProductsComponent,
+    StatisticsComponent,
+    CoupensComponent,
+    PagesComponent,
+    MediaComponent,
+    SettingsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
